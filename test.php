a02c3370d4bb92396df26e6e47acde1ffd9e3622
divya-tembhre <47885200+divya-tembhre@users.noreply.github.com>
2019-02-22 14:53:00 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+//phpinfo();
+
+//imagedestroy($im);
+
+//exit;   
+
+//for ($i=1,$j=1;$i<10 && $j<10; $i++, $j++) 
+
+for($i=0;$i<=3;$i++)
+{
+  //echo $i;
+  echo "<br>";
+ // return 0;
+}
+
+
+
+echo "<br>";
+
+$i=1;
+while($i=5){
+    echo $i;
+    echo "<br>";
+    $i++;
+}
+
+
+?>
+
+
+ 
