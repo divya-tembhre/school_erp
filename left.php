ca0c6ce9366ef0634c383598e23bafb62d24e752
divya-tembhre <47885200+divya-tembhre@users.noreply.github.com>
2019-02-23 10:19:16 +0530

    Add files via upload

---
+++ left.php
@@ -0,0 +1,101 @@
+<div class="nav-left-sidebar sidebar-dark">
+            <div class="menu-list">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav flex-column">
+                            <li class="nav-divider">
+                                Menu
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
+                                <div id="submenu-1" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="index.php">Dashboard</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="profile.php">Profile</a>
+                                        </li>
+                                        
+                                    </ul>
+                                </div>
+                            </li>
+							
+                             <li class="nav-item ">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>class <span class="badge badge-success">6</span></a>
+                                <div id="submenu-2" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="add_class.php">Add class</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="view_class.php">View class</a>
+                                        </li>
+										                                      
+                                    </ul>
+                                </div>
+                            </li>
+							
+							<li class="nav-item ">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-4" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>section <span class="badge badge-success">6</span></a>
+                                <div id="submenu-4" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="add_section.php">Add section</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="view_section.php">View section</a>
+                                        </li> 
+										                                         
+                                    </ul>
+                                </div>
+                            </li>
+							
+                               <li class="nav-item ">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-6" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>student <span class="badge badge-success">6</span></a>
+                                <div id="submenu-6" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="add_student.php">Add student</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="view_student.php">View student</a>
+                                        </li> 
+										                                         
+                                    </ul>
+                                </div>
+                            </li>
+                               
+                             <li class="nav-item ">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-3" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Inventory <span class="badge badge-success">6</span></a>
+                                <div id="submenu-3" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="add_inventory.php">Add Inventory</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="view_inventory.php">View Inventory</a>
+                                        </li> 
+										                                          
+                                    </ul>
+                                </div>
+                            </li>
+                               
+                             
+                               
+                             
+							
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
